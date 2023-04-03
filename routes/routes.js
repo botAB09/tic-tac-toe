b7686a7652f83c3cadb7d62c7d2869a1875e250c
Kunal <bhandarik667@gmail.com>
2023-04-03 15:55:17 +0530

    singelton class db.util:

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,6 @@ game = require('../controllers/game-controller'),
 gamestats = require('../controllers/userstats-controller');
 
 //TODO how to do hashing in routes and its usage 
-//TODO singelton progm..
 router.get('/',home);
 router.get('/game',game);
 router.post('/gamestats',gamestats);
