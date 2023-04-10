f6a967f50a85491010e5f4aaa5315a1780219901
Kunal <bhandarik667@gmail.com>
2023-04-10 12:21:04 +0530

    merged new socket server side logic

--- utility/websocket.utility.js
+++ utility/websocket.utility.js
@@ -1,36 +1,30 @@
-/**
- * to be updated : 
- * --new features for server side code 
- * --adding new socket handler for client side game 
- * --removing client side code from front end  
- */
-
 const db = require('./db.utility');
+
 /**
  * websocket utility class :
  *      -- creates the game 
+ *              creates the game by assigning opponent , symbol and username to each socket connected 
+ *                  -- stores the player data in key:value pair 
+ *                  -- each user is stored in "player" object with key as socket.id 
+ *              properties of each player[socket.id] object :
+ *                  -- symbol: contains the user symbol for the board
+ *                  -- opponent: is null initially ; when another user connects then available sockets are connected 
+ *                  -- socket: stores the socket object 
+ *                  -- username: stores the username of each connected user
+ * 
  *      -- manages socket and emits websocket event for each connected user
+ *          socketHandler() :
+ *              main driver function of websockets ;
+ *                  -- createGame() function for both the connected user is called ; then 
+ *                  -- getOpponent() checks for available opponents
+ *                      -- if yes then , socket event is emitted to begin the game and load the game board 
+ *                  -- if player makes a valid move , then socket event is emitted to display the move 
+ *          
  */
-
-//TODO garbage collector in static function methods ; explain class bit more ; functional comments should be concise 
-//TODO net library , implementation of socket TCP socket ; TCP protocol , architecture , HTTPS , HTTP 
-//TODO packet loss ; spotify read !!
-//TODO 
 class SOCKETUTIL{
     static opponent;
     static player = {};
-    /**
-     * 
-     * @param {object} socket 
-     * creates the game by assigning opponent , symbol and username to each socket connected 
-     *  -- stores the player data in key:value pair 
-     *  -- each user is stored in "player" object with key as socket.id 
-     *     properties of each player[socket.id] object :
-     *          -- symbol: contains the user symbol for the board
-     *          -- opponent: is null initially ; when another user connects then available sockets are connected 
-     *          -- socket: stores the socket object 
-     *          -- username: stores the username of each connected user
-     */
+
 
     static createGame(socket){
         console.log(SOCKETUTIL.opponent);
@@ -38,10 +32,12 @@ class SOCKETUTIL{
             symbol : 'X',
             opponent : SOCKETUTIL.opponent,
             socket : socket,
-            username : ''
+            username : '',
+            turn : true
         }
         if(SOCKETUTIL.opponent){
             SOCKETUTIL.player[socket.id].symbol = 'O';
+            SOCKETUTIL.player[socket.id].turn = false;
             SOCKETUTIL.player[SOCKETUTIL.opponent].opponent = socket.id;
             SOCKETUTIL.opponent = null;
         }
@@ -49,52 +45,143 @@ class SOCKETUTIL{
             SOCKETUTIL.opponent = socket.id;
         }
     }
-    /**
-     * 
-     * @param {object} socket 
-     * @returns the socket object of the opponent property of "player" object
-     */
+
     static getOpponent(socket){
         if(SOCKETUTIL.player[socket.id].opponent){
             return SOCKETUTIL.player[SOCKETUTIL.player[socket.id].opponent].socket;
         }
         return false;
     }
+
     /**
      * 
-     * @param {object} socket 
-     * main driver function of websockets ;
-     * -- createGame() function for both the connected user is called ; then 
-     * -- getOpponent() checks for available opponents
-     *    -- if yes then , socket event is emitted to begin the game and load the game board 
-     * -- if player makes a valid move , then socket event is emitted to display the move 
-     * 
+     * @param {object} gameBoard contains position each player symbol on the game board
+     * @returns true if winner is found ; else false 
      */
+    static isWinner(gameBoard){
+        const matches = ['XXX','OOO'];
+        const winCombination = [
+            gameBoard.cell1+gameBoard.cell2+gameBoard.cell3,
+            gameBoard.cell4+gameBoard.cell5+gameBoard.cell6,
+            gameBoard.cell7+gameBoard.cell8+gameBoard.cell9,
+            gameBoard.cell1+gameBoard.cell5+gameBoard.cell9,
+            gameBoard.cell3+gameBoard.cell5+gameBoard.cell7,
+            gameBoard.cell1+gameBoard.cell4+gameBoard.cell7,
+            gameBoard.cell2+gameBoard.cell5+gameBoard.cell8,
+            gameBoard.cell3+gameBoard.cell6+gameBoard.cell9
+        ];
+        for(const element of winCombination){
+            if(element===matches[0] || element===matches[1]){
+                return true;           
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param {object} gameBoard contains position each player symbol on the game board
+     * @returns true if game is drawn ; else false
+     */
+    static isDraw(gameBoard){
+        const val = Object.values(gameBoard);
+        for(const element of val){
+            if(element === ""){
+                return false;
+            }
+        }
+        return true;
+    }
+
     static socketHandler(socket){
         console.log(`A User Connected ${socket.id}`);
         SOCKETUTIL.createGame(socket); 
         if(SOCKETUTIL.getOpponent(socket)){
             socket.emit('game.begin',{
-                symbol : SOCKETUTIL.player[socket.id].symbol
+                symbol : SOCKETUTIL.player[socket.id].symbol,
+                turn : SOCKETUTIL.player[socket.id].turn
             })
             SOCKETUTIL.getOpponent(socket).emit('game.begin',{
-                symbol : SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].symbol
+                symbol : SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].symbol,
+                turn : SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].turn
             })
         }
+
         socket.on('player.move',player_postition=>{
-            console.log(SOCKETUTIL.player[socket.id]);
             console.log("A move was made",player_postition);
-            socket.emit('move.made',player_postition);
-            SOCKETUTIL.getOpponent(socket).emit('move.made',player_postition);
+            if(SOCKETUTIL.player[socket.id].turn){
+                SOCKETUTIL.player[socket.id].turn = false;
+                SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].turn = true; 
+                socket.emit('move.made',{
+                    position: player_postition,
+                    turn: SOCKETUTIL.player[socket.id].turn,
+                    symbol: SOCKETUTIL.player[socket.id].symbol
+                });
+                SOCKETUTIL.getOpponent(socket).emit('move.made',{
+                    position: player_postition,
+                    turn: SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].turn,
+                    symbol: SOCKETUTIL.player[socket.id].symbol
+                });         
+            }
         })
+
+        socket.on("game.state.check",gameBoard=>{
+            console.log(gameBoard);
+            console.log(socket.id);
+            if(SOCKETUTIL.isWinner(gameBoard)){
+                socket.emit("game.state",{
+                    Win: true,
+                    Loss: false,
+                    Draw: false,
+                    turn: SOCKETUTIL.player[socket.id].turn
+                });
+                SOCKETUTIL.getOpponent(socket).emit("game.state",{
+                    Win: false,
+                    Loss: true,
+                    Draw: false,
+                    turn: SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].turn
+                });                                
+            }
+            else if(SOCKETUTIL.isDraw(gameBoard)){
+                socket.emit("game.state",{
+                    Win: false,
+                    Loss: false,
+                    Draw: true,
+                    turn: SOCKETUTIL.player[socket.id].turn
+                });
+                SOCKETUTIL.getOpponent(socket).emit("game.state",{
+                    Win: false,
+                    Loss: false,
+                    Draw: true,
+                    turn: SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].turn
+                });   
+            }  
+            else{
+                socket.emit("game.state",{
+                    Win: false,
+                    Loss: false,
+                    Draw: false,
+                    turn: SOCKETUTIL.player[socket.id].turn
+                });
+                SOCKETUTIL.getOpponent(socket).emit("game.state",{
+                    Win: false,
+                    Loss: false,
+                    Draw: false,
+                    turn: SOCKETUTIL.player[SOCKETUTIL.getOpponent(socket).id].turn                   
+                });   
+            }   
+
+        }) 
+
         socket.on("username",username=>{
             SOCKETUTIL.player[socket.id].username = username;
         })
         socket.on("game.end",gameState=>{
             db.addUser(SOCKETUTIL.player[socket.id],gameState);
         })
-        socket.on("disconnect",data=>{
-            console.log(data);
+
+        socket.on("disconnect",disconn_data=>{
+            console.log(disconn_data);
             if(SOCKETUTIL.getOpponent(socket)){
                 SOCKETUTIL.getOpponent(socket).emit('opponent.left');
             }
