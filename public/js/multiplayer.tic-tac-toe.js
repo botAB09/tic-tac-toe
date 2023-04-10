ab314f0c1c1266b6c2d3e93aab880ce5ec7e173e
Kunal <bhandarik667@gmail.com>
2023-04-10 12:20:36 +0530

    merged new socket client side logi

--- public/js/multiplayer.tic-tac-toe.js
+++ public/js/multiplayer.tic-tac-toe.js
@@ -1,15 +1,8 @@
 const 
-socket = io(),
-matches = ['XXX','OOO'];
+socket = io();
 
-let
-turn = true,
-symbol;
-
-//TODO create a constants file , string literals 
-//TODO add logic to server side ....
-
-function renderTurnMessage(){
+//render turn message to the connected clients
+function renderTurnMessage(turn){
     if(!turn){
         $("#turn").text("Your Opponent's Turn");
         $('.cell').attr('disabled', true); 
@@ -19,87 +12,58 @@ function renderTurnMessage(){
         $('.cell').removeAttr('disabled');     
     }
 }
+
+//emits socket event to server , when player makes a move
 function makeMove(){
-    if(!turn || $(this).text().length){
+    if($(this).text().length){
         return ;
     }
-    socket.emit('player.move',{
-        symbol : symbol,
-        position : this.id
-    })    
+    socket.emit('player.move',this.id)    
 }
 
-/**
- * 
- * @returns object containing state of the board (where 'X' and 'O' are located)
- */
+//creates a board state object comprising of player symbol position on the board
 function getBoardState(){
-    const obj = {};
+    const boardState = {};
     $('.cell').each(function(index){
-        obj[$(this).attr('id')]= $(this).text();
+        boardState[$(this).attr('id')]= $(this).text();
     });
-    return obj;
+    return boardState;
 }
-//TODO dont use vague terms 
-function checkBoard(){
-    const obj = getBoardState();
 
-    const winCombination = [
-        obj.a1+obj.a2+obj.a3,
-        obj.a4+obj.a5+obj.a6,
-        obj.a7+obj.a8+obj.a9,
-        obj.a1+obj.a5+obj.a9,
-        obj.a3+obj.a5+obj.a7,
-        obj.a1+obj.a4+obj.a7,
-        obj.a2+obj.a5+obj.a8,
-        obj.a3+obj.a6+obj.a9
-    ];
-    for(const element of winCombination){
-        if(element===matches[0] || element===matches[1]){
-            return true;           
-        }
-    }
-    return false;
-}
-function checkDraw(){
-    const obj = getBoardState();
-    const val = Object.values(obj);
-    for(const element of val){
-        if(element === ""){
-            return false;
-        }
-    }
-    return true;
-}
 socket.on("connect", () => {
     console.log(socket.id);
 });
-socket.on("game.begin",data=>{
-    symbol = data.symbol;
-    $("#symbol").text(`Your Symbol : ${data.symbol}`);
-    turn = (data.symbol === 'X');
-    renderTurnMessage();
+socket.on("game.begin",playerData=>{
+    console.log(playerData)
+    $("#symbol").text(`Your Symbol : ${playerData.symbol}`);
+    renderTurnMessage(playerData.turn);
 });
 socket.on("move.made",data=>{
+    console.log(data);
     $("#"+data.position).text(data.symbol);
-    turn = (data.symbol !== symbol);
-    if(!checkBoard()){
-        if(checkDraw()){
-            $("#turn").text("Draw!... reload for New Game!");
-            $('.cell').attr('disabled', true);
-            socket.emit("game.end","draw");
-            return ;
-        }
-        renderTurnMessage();
-        return ;
+    const gameBoard = getBoardState();
+    if(!data.turn){
+        socket.emit("game.state.check",gameBoard);
     }
-    if(turn){
+})
+socket.on("game.state",gameState=>{
+    console.log(gameState);
+    if(gameState.Win){
+        $("#turn").text("You Win!");
+        socket.emit("game.end","Win");
+    }
+    else if(gameState.Loss){
         $("#turn").text("You Loose!");
         socket.emit("game.end","Loss");
     }
+    else if(gameState.Draw){
+        $("#turn").text("Draw!... reload for New Game!");
+        $('.cell').attr('disabled', true);
+        socket.emit("game.end","draw");       
+    }
     else{
-        $("#turn").text("You Win!");
-        socket.emit("game.end","Win");
+        renderTurnMessage(gameState.turn);
+        return;
     }
     $('.cell').attr('disabled', true);
 })
