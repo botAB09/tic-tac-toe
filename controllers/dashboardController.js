ee5ff44cc77641f58b42c44a6e4d2f3ab37e0d3e
Kunal <bhandarik667@gmail.com>
2023-04-21 12:14:35 +0530

    added user stats box

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,8 +2,12 @@ const path = require("path"),
   db = require("../utilities/db.utility");
 
 const dashboardView = async function (req, res) {
-  res.render("user.dashboard.ejs", {
+  const userStats = await fetchUserStatistics(req.session.username);
+  res.render('user.dashboard.ejs', {
     username: req.session.username,
+    win: userStats.Win ,
+    loss: userStats.Loss,
+    draw: userStats.Draw,
   });
 };
 
@@ -15,16 +19,14 @@ const pvpGame = async function (req, res) {
   res.render("pvp.ejs");
 };
 
-const fetchUserStatistics = async function (req, res) {
+const fetchUserStatistics = async function (username) {
   try {
-    const username = req.session.username;
     const gamestatistics = await db.getUserStats(username);
-    const userStats = {
+    return {
       Win: gamestatistics[0].Win,
       Loss: gamestatistics[0].Loss,
       Draw: gamestatistics[0].Draw,
     };
-    res.send(userStats);
   } catch (err) {
     console.log("Error: retrieving game statistics of the user", err);
   }
