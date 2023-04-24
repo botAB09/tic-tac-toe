e517c6e0463af0df143634ba198111839c3d79f8
Kunal <bhandarik667@gmail.com>
2023-04-24 11:46:41 +0530

    schema updated

--- database/schema/db.schema.js
+++ database/schema/db.schema.js
@@ -1,47 +1,47 @@
 /**
- *@param {object} client validates database schema and creates the rules for database with following properties 
- * ---properties : 
+ *@param {object} client validates database schema and creates the rules for database with following properties
+ * ---properties :
  *      -username  : "string"
  *      -win : "int"
  *      -loss : "int"
  *      -draw : "int"
  */
-const createGameSchema = async(client)=>{
-    await client.db(process.env.dbname).createCollection('gameUsers',{
-        validator: {
-            $jsonSchema: {
-                bsonType: "object",
-                title: "Tic Tac Toe Object Validation",
-                required: ["username"],
-                properties: {
-                    username: {
-                        bsonType: "string",
-                        description: "username must be string and is required"
-                    },
-                    email: {
-                        bsonType: "string"
-                    },
-                    password: {
-                        bsonType: "string"       
-                    },
-                    Win: {
-                        bsonType: "int",
-                        minimum: 0,
-                        description: "Win must be integer"
-                    },
-                    Loss: {
-                        bsonType: "int",
-                        minimum: 0,
-                        description: "Loss must be integer"
-                    },
-                    Draw: {
-                        bsonType: "int",
-                        minimum: 0,
-                        description: "Draw must be integer"
-                    }
-                }
-            }
-        }
-    })
-}
+const createGameSchema = async (client) => {
+  await client.db(process.env.dbname).createCollection("gameUsers", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "Tic Tac Toe Object Validation",
+        required: ["username"],
+        properties: {
+          username: {
+            bsonType: "string",
+            description: "username must be string and is required",
+          },
+          email: {
+            bsonType: "string",
+          },
+          password: {
+            bsonType: "string",
+          },
+          Win: {
+            bsonType: "int",
+            minimum: 0,
+            description: "Win must be integer",
+          },
+          Loss: {
+            bsonType: "int",
+            minimum: 0,
+            description: "Loss must be integer",
+          },
+          Draw: {
+            bsonType: "int",
+            minimum: 0,
+            description: "Draw must be integer",
+          },
+        },
+      },
+    },
+  });
+};
 module.exports = createGameSchema;
